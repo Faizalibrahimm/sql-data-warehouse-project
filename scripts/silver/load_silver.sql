f682884689a9ef051b7f20304ecca8b6d050b944
Faizalibrahimm <laziaf20@gmail.com>
2025-10-30 16:25:54 +0700

    Create load_silver.sql

---
+++ scripts/silver/load_silver.sql
@@ -0,0 +1,195 @@
+/*
+==================================================
+Load Silver Layer (Bronze -> Silver)
+==================================================
+Script Purpose:
+	This script performs Cleaning and ETL from the bronze schema to the silver schema.
+	Run the scripts one by one in order from the top and when running the script, 
+	also understand the code from the “ check_data_quality.sql " file in the tests folder.
+*/
+
+
+-- Load Table 'crm_cust_info' from bronze schema
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date)
+SELECT
+	t.cst_id,
+	t.cst_key,
+	TRIM(t.cst_firstname) AS cst_firstname,
+	TRIM(t.cst_lastname) AS cst_lastname,
+	CASE WHEN UPPER(cst_marital_status) = 'S' THEN 'Single'
+		 WHEN UPPER(cst_marital_status) = 'M' THEN 'Married'
+		 ELSE 'n/a'
+	END AS cst_marital_status,
+	CASE WHEN UPPER(cst_gndr) = 'F' THEN 'Female'
+		 WHEN UPPER(cst_gndr) = 'M' THEN 'Male'
+		 ELSE 'n/a'
+	END AS cst_gndr,
+	t.cst_create_date
+FROM (
+	SELECT 
+		*,
+		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date desc) AS flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+	) t
+WHERE t.flag_last = 1;
+
+SELECT * FROM silver.crm_cust_info;
+
+
+-- Load Table 'crm_prd_info' from bronze schema
+-- Before that, delete the existing ‘crm_prd_info’ table because
+-- there is a change in the data type in the ‘prd_start_dt’ and ‘prd_end_dt’ columns to date type
+DROP TABLE IF EXISTS silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info (
+	prd_id INT,
+	cat_id VARCHAR(50),
+	prd_key VARCHAR(50),
+	prd_nm VARCHAR(50),
+	prd_cost INT,
+	prd_line VARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt)
+SELECT 
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key,
+	prd_nm,
+	COALESCE(prd_cost, 0) AS prd_cost,	--replace ISNULL
+	CASE UPPER(TRIM(prd_line))
+		 WHEN 'M' THEN 'Mountain'
+		 WHEN 'R' THEN 'Road'
+		 WHEN 'S' THEN 'Other Sales'
+		 WHEN 'T' THEN 'Touring'
+		 ELSE 'n/a'
+	END AS prd_line,
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	CAST((LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 days') AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info;
+
+SELECT * FROM silver.crm_prd_info;
+
+
+-- Load Table 'crm_sales_details' from bronze schema
+-- Before that, delete the existing ‘crm_sales_details’ table because there is a new column addition
+DROP TABLE IF EXISTS silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details(
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price)
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE WHEN sls_order_dt = 0 OR LENGTH(CAST(sls_order_dt AS TEXT)) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt,
+	CASE WHEN sls_ship_dt = 0 OR LENGTH(CAST(sls_ship_dt AS TEXT)) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt,
+	CASE WHEN sls_due_dt = 0 OR LENGTH(CAST(sls_due_dt AS TEXT)) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt,
+		CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * sls_price
+		 THEN sls_quantity * ABS(sls_price)
+		 ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE WHEN sls_price IS NULL OR sls_price <= 0
+		 THEN sls_sales / NULLIF(sls_quantity, 0)
+		 ELSE sls_price
+	END AS sls_price
+FROM bronze.crm_sales_details;
+
+SELECT * FROM silver.crm_sales_details;
+
+
+-- Load Table 'erp_cust_az12' from bronze schema
+INSERT INTO silver.erp_cust_az12(
+	cid,
+	bdate,
+	gen)
+SELECT 
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
+		 ELSE cid
+	END AS cid,
+	CASE WHEN bdate > CURRENT_DATE THEN NULL
+		 ELSE bdate
+	END AS bdate,
+	CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+		 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+		 ELSE 'n/a'
+	END AS gen
+FROM bronze.erp_cust_az12;
+
+SELECT * FROM silver.erp_cust_az12;
+
+
+-- Load Table 'erp_loc_a101' from bronze schema
+INSERT INTO silver.erp_loc_a101(
+	cid,
+	cntry)
+SELECT 
+	REPLACE(cid, '-', '') AS cid,
+	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+		 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	ELSE TRIM(cntry)
+	END AS cntry
+FROM bronze.erp_loc_a101;
+
+SELECT * FROM silver.erp_loc_a101;
+
+
+-- Load Table 'erp_px_cat_g1v2' from bronze schema
+INSERT INTO silver.erp_px_cat_g1v2(
+	id,
+	cat,
+	subcat,
+	maintenance)
+SELECT 
+	id,
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2;
+
+SELECT * FROM silver.erp_px_cat_g1v2;
